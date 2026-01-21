b7bfde6418281d101bc525defd14074a1608a8a7
Haohan Lin <guameezia@gmail.com>
2026-01-21 23:55:01 +0800

    Configure Vite for GitHub Pages user site

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/guameezia-website/', // GitHub Pages base path; set to '/' if using a custom domain
+  base: '/',
+  build: {
+    outDir: 'docs',
+  },
 })
